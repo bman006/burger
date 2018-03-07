124fcf58012b6e52a9ab08bfba8b160472b3108a
Brian Parkes <parkesbrianj@gmail.com>
2018-03-06 21:55:31 -0600

    Setting up controller, on line 140

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,36 @@
+var express = require(`express`);
+var router = express.Router();
+var burger = require(`../models/burger.js`);
+
+//Home page
+router.get("/", function(req, res) {
+    //Get all burger data from database
+    burger.all(function(data) {
+        //change format of data for use in handlebars
+
+        //Render page
+    });
+});
+
+//Add a burger to the page
+router.post("/api/:burger", function(req, res) {
+    //Add new burger to the database
+    burger.create(req.params.burger, function(data) {
+        //Send back the full row of data for the new burger
+        res.json({ 
+            id: data.id,
+            burger_name: data.burger_name,
+            devoured: data.devoured
+        });
+    });
+});
+
+//Devour a burger
+router.put("/api/devour/:id", function(req, res) {
+    //Devour the burger, changing the corresponding boolean value in the table
+    burger.update(req.params.id, function(data) {
+        //Send back the now updated row of data for the burger
+    });
+});
+
+module.exports = router;
