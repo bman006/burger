aa71d2f1cf74a544acc754f6ee8f5bb8a1c5183d
Brian Parkes <parkesbrianj@gmail.com>
2018-03-10 10:08:32 -0600

    Updated sql files

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
