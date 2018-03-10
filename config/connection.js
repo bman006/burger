d8cc82b5c3e8b4105dec45de5206a24cf622db61
Brian Parkes <parkesbrianj@gmail.com>
2018-03-10 09:52:42 -0600

    Added missing connection code

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var keys = require(`../keys.js`);
 var mysql      = require('mysql');
 
 if (process.env.JAWSDB_URL) {
-
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
   var connection = mysql.createConnection({
