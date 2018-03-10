75f76bf56d538471c7989466ff37ddb5a2651343
Brian Parkes <parkesbrianj@gmail.com>
2018-03-10 09:49:45 -0600

    Added Jaws DB connection info

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
 var keys = require(`../keys.js`);
-
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : keys.sqlLogin.sqlUser,
-  password : keys.sqlLogin.sqlPassword,
-  database : 'burgers_db'
-});
+
+if (process.env.JAWSDB_URL) {
+
+}
+else {
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : keys.sqlLogin.sqlUser,
+    password : keys.sqlLogin.sqlPassword,
+    database : 'burgers_db'
+  });
+}
  
 connection.connect();
- 
 module.exports = connection;
